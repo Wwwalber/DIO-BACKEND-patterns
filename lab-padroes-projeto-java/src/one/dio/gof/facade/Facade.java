99a926c50185bb681c600655bcc9d94a77680264
walber <walberoc@gmail.com>
2024-07-25 05:20:42 -0300

    iniciando a facade

---
+++ lab-padroes-projeto-java/src/one/dio/gof/facade/Facade.java
@@ -0,0 +1,10 @@
+package one.dio.gof.facade;
+
+
+// Facade para expor intenfaces mais complexas
+public class Facade {
+
+    public void migrarClient(String nome, String cep){
+        // como integrar o subsistema externo
+    }
+}
